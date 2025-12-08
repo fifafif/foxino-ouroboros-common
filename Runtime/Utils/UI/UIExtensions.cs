d15bb2f1227c3e23d33f5aab5ce3d74078ff7f6c
fifafif <fifav@seznam.cz>
2025-12-08 10:02:11 +0100

    UI exension

--- Runtime/Utils/UI/UIExtensions.cs
+++ Runtime/Utils/UI/UIExtensions.cs
@@ -5,6 +5,17 @@ namespace Ouroboros.Common.Utils.UI
 {
     public static class UIExtensions
     {
+        public static void RemoveAndAddOnClickAction(this Button button, UnityAction onClickAction)
+        {
+            if (button == null)
+            {
+                return;
+            }
+
+            button.onClick.RemoveListener(onClickAction);
+            button.onClick.AddListener(onClickAction);
+        }
+
         public static bool RectOverlaps(this RectTransform rectTrans1, RectTransform rectTrans2)
         {
             // Rect rect1 = new Rect(rectTrans1.position.x, rectTrans1.position.y, rectTrans1.rect.width, rectTrans1.rect.height);
