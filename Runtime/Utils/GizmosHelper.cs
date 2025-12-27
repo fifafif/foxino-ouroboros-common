b05bf007933e54d9b8b748deba5e44183496e1c9
fifafif <fifav@seznam.cz>
2025-12-27 16:36:05 +0700

    GizmosHelper update

--- Runtime/Utils/GizmosHelper.cs
+++ Runtime/Utils/GizmosHelper.cs
@@ -4,6 +4,9 @@ namespace Ouroboros.Common.Utils
 {
     public static class GizmosHelper
     {
+        private static Color origGizmosColor;
+        private static Matrix4x4 origGizmosMatrix;
+
         public static void DrawWireCube(Vector3 position, float size, Color color)
         {
             DrawWireCube(position, new Vector3(size, size, size), color);
@@ -31,6 +34,40 @@ namespace Ouroboros.Common.Utils
             Gizmos.color = color;
             Gizmos.DrawLine(from, to);
             Gizmos.color = origColor;
+        }
+
+        public static void SetColor(Color color)
+        {
+            origGizmosColor = Gizmos.color;
+            Gizmos.color = color;
+        }
+
+        public static void ResetColor()
+        {
+            Gizmos.color = origGizmosColor;
+        }
+
+        public static void SetMatrix(Matrix4x4 matrix)
+        {
+            origGizmosMatrix = Gizmos.matrix;
+            Gizmos.matrix = matrix;
+        }
+
+        public static void ResetMatrix()
+        {
+            Gizmos.matrix = origGizmosMatrix;
+        }
+
+        public static void SetColorAndMatrix(Color color, Matrix4x4 matrix)
+        {
+            SetColor(color);
+            SetMatrix(matrix);
+        }
+
+        public static void ResetColorAndMatrix()
+        {
+            ResetColor();
+            ResetMatrix();
         }
     }
 }
